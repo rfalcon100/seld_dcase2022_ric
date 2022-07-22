21b590c7f9d128809ec337b175e9cfd996904a96
Falcon Perez Ricardo <ricardo.falconperez@aalto.fi>
2022-07-22 17:47:23 +0300

    Towards plots for speed, accelration and polyphony
    Added soem code copied from GANtestbed for the plots of the datasets.
    Looks ok, but it is still not ready.

--- analysis.py
+++ analysis.py
@@ -63,7 +63,6 @@ def plot_histograms_bivariate_azi_ele(targets, threshold=0.5):
     # plots.plot_distribution_azi_ele(active_targets_all_classes, type='hist', log_scale=False, title='Original', cmin=1, gridsize=100)
 
 
-
 def plot_histograms_active_per_class(all_labels: List, all_labels_test: List = None, all_labels_sim: List = None,
                                      splits=['dev-train', 'dev-test', 'synth-set'], detection_threshold=0.5,
                                      format_use_log=True, filename='dcase22_hist_active_per_class',
@@ -102,7 +101,6 @@ def plot_histograms_active_per_class(all_labels: List, all_labels_test: List = N
                     dict_of_dectections[cls] += len(this_count_detections)
                 else:
                     dict_of_dectections[cls] = len(this_count_detections)
-
 
     # df = pd.DataFrame.from_dict(all_count_detections, orient='index', columns=['count'])
     df = pd.DataFrame(list(all_count_detections.items()))
@@ -152,6 +150,137 @@ def plot_histograms_active_per_class(all_labels: List, all_labels_test: List = N
     plt.tight_layout()
     plt.savefig(f'./figures/{filename}.pdf')
     plt.savefig(f'./figures/{filename}.png')
+    plt.show()
+
+def plot_histograms_polyphony(targets: List, detection_threshold=0.5, format_use_log=False, chunk_size=128):
+    # Here I test a small plot to get histograms of active sources per chunk
+    # This is the polyphony
+    # 17.03 it kinda works now
+    y = torch.concat(targets, dim=-1)[None,...]   # concat over frames
+    #n_examples = y.shape[0]
+    n_examples = len(targets)
+
+    ####chunk_size = 128  # This how big the chunk is, if = 128, then we dont split the example at all
+    all_active_sources = []
+    n_chunks = int(y.shape[-1] / chunk_size)  # n_chunks for each example
+    #y_chunks = torch.chunk(y, chunks=n_chunks, dim=-1)
+    pad_size = chunk_size - y.shape[-1] % chunk_size
+    padder = nn.ConstantPad2d(padding=(0, pad_size, 0, 0), value=0.0)  # Hack to pad 6001 --> 6032, for full files only
+    y_chunks = torch.chunk(padder(y), chunks=n_chunks + 1, dim=-1)
+
+    y = torch.cat(y_chunks, dim=0)
+    norma = torch.linalg.vector_norm(y, ord=2, dim=-3)  # [n_classes, frames]
+    mask_active_sources = (norma > detection_threshold).any(dim=-1)
+
+    for i in range(y.shape[0]):
+        n_active_sources = len(mask_active_sources[i].nonzero(as_tuple=False))
+        all_active_sources.append(n_active_sources)
+    all_active_sources = torch.tensor(all_active_sources)
+
+    # Plot
+    fig = plt.figure()
+    plt.hist(all_active_sources.detach().cpu().numpy(), bins=[0, 1, 2, 3, 5, 6, 7, 8], density=True)
+    ax = plt.gca()
+    ax.set_title(f'n_examples = {n_examples} chunk_size = {chunk_size}')
+    plt.show()
+
+    return
+    # Ths is not ready
+
+    # TODO This is not ready
+    # Plot with sns
+    df = pd.DataFrame(list(all_active_sources.items()))
+    df.columns = ['polyphony', 'count']
+    #df['polyphony'] = sound_event_classes_2022
+    #split = [splits[0]] * len(sound_event_classes_2022)
+    #df['split'] = split
+
+    # Horizontal, looks nice
+    f, ax = plt.subplots(figsize=(18, 7))
+    g = sns.barplot(x="polyphony", y="count", data=df, palette='magma')
+    # g = sns.catplot(x="class_name", kind='count', data=df, hue='split', palette='magma')
+    # sns.despine(left=False, bottom=False)
+    if format_use_log:
+        g.set_yscale("log")
+        g.set_yticks([10 ** x for x in range(6)])
+        # g.set_xticklabels(['0','a','b','c','d','e'])
+    g.set_xticklabels(g.get_xticklabels(), rotation=35)
+    plt.tight_layout()
+    #plt.savefig(f'./figures/{filename}.pdf')
+    #plt.savefig(f'./figures/{filename}.png')
+    plt.show()
+
+def plot_speed_and_acceleration(targets, format_use_log=False, chunk_size=128):
+    # Based on test_friday from GANtestbe
+    # So this is a test to get the velocity using real data
+    # I think it works ok, the plot and the numbers look like they match
+    # This is good
+
+    n_class = 11
+    plot_range = range(3500, 4000)
+    y = torch.concat(targets, dim=-1)[None,...]   # concat over frames
+    #n_examples = y.shape[0]
+    n_examples = len(targets)
+
+    y_velocity = torch.diff(y, dim=-1)    # Optional , multiply y_velocity * 10 to get meters/sec
+    y_acceleration = torch.diff(y_velocity, dim=-1)
+    y_speed = torch.linalg.vector_norm(y_velocity, ord=2, dim=-3)  # Magnitude over channels
+
+    plots.plot_labels(y, title='y')
+    plots.plot_labels_cross_sections(y, title='y')
+
+    # Spherical coords
+    y_spherical = np.zeros_like(y)
+    for cc in range(y_spherical.shape[1]):
+        tmp = utils.vecs2dirs(y[:, cc, :].squeeze().transpose(1, 0), include_r=True, use_elevation=True)
+        y_spherical[:, cc, ::] = tmp.transpose(1, 0)
+    y_spherical = torch.from_numpy(y_spherical)
+    plots.plot_labels(y_spherical, plot_cartesian=False)
+
+    fig = plt.figure()
+    n_class = 2  # Manually, select an active class
+    n_coord = 0  # Manually, select x,y or z
+    plt.plot(y[n_coord, n_class, :], '-r')
+    plt.plot(y_velocity[n_coord, n_class, :], '--b')
+    plt.plot(y_speed[n_class, :], '-g')
+    plt.show()
+
+    # Zoom in:
+    fig = plt.figure()
+    plt.plot(y[0, n_class, plot_range], '-r')
+    plt.plot(y_velocity[0, n_class, plot_range], '--b')
+    # plt.plot(y_acceleration[0, n_class, :], ':k')
+    plt.plot(y_speed[n_class, plot_range], '-g')
+    plt.show()
+
+    # And histograms
+    # Another option is to not do it by class
+    n_class = range(0, 13)  # This is we want all classes
+    y_speed_truncated = y_speed[n_class, :]
+    y_speed_truncated = y_speed_truncated[y_speed_truncated > 0.001]
+    y_speed_truncated[y_speed_truncated > 1] = 1
+
+    fig = plt.figure()
+    plt.hist(y_speed_truncated.detach().cpu().numpy(), density=True, bins=30)
+    # plt.violinplot(y_speed, vert=False, showextrema=False, showmedians=True)
+    ax = plt.gca()
+    # ax.set_xlim([0.0, 0.1])
+    ax.set_title('Speed')
+    plt.show()
+
+    # TODO This is not ready
+    y_acceleration_mag = torch.linalg.vector_norm(y_acceleration, ord=2, dim=-3)
+    n_class = range(0, 13)  # This is we want all classes
+    y_acceleration_truncated = y_acceleration_mag[n_class, :]
+    y_acceleration_truncated = y_acceleration_truncated[y_acceleration_truncated > 0.001]
+    y_acceleration_truncated[y_acceleration_truncated > 1] = 1
+
+    fig = plt.figure()
+    plt.hist(y_acceleration_truncated.detach().cpu().numpy(), density=True, bins=30)
+    # plt.violinplot(y_speed, vert=False, showextrema=False, showmedians=True)
+    ax = plt.gca()
+    # ax.set_xlim([0.0, 0.1])
+    ax.set_title('Accelreation')
     plt.show()
 
 
@@ -218,9 +347,19 @@ def main():
 
     print('Start analysis')
     dataset_train, dataset_valid, dataset_synth = None, None, None  # Free memory
-    #plot_histograms_bivariate_azi_ele(targets_valid_flat)
+    #####plot_histograms_bivariate_azi_ele(targets_valid_flat)
 
-    plot_histograms_active_per_class(targets_train, targets_valid, targets_synth, detection_threshold=0.5)
+    #####plot_histograms_active_per_class(targets_train, targets_valid, targets_synth, detection_threshold=0.5)
+    #####plt.savefig('./figures/figure_01_active_per_class.pdf')
+    #####plt.savefig('./figures/figure_01_active_per_class.png')
+    print('End of analysis')
+
+    plot_histograms_polyphony(targets_train, detection_threshold=0.5, format_use_log=False, chunk_size=128)
+    plt.savefig('./figures/figure_01_active_per_class.pdf')
+    plt.savefig('./figures/figure_01_active_per_class.png')
+    print('End of analysis')
+
+    plot_speed_and_acceleration(targets_train)
     plt.savefig('./figures/figure_01_active_per_class.pdf')
     plt.savefig('./figures/figure_01_active_per_class.png')
     print('End of analysis')
@@ -232,6 +371,8 @@ if __name__ == '__main__':
     ./configs/run_debug.yaml
     --dataset_trim_wavs
     -1
+    --dataset_backend
+    baseline
     --dataset_root
     /m/triton/scratch/work/falconr1/sony/data_dcase2022
     /m/triton/scratch/work/falconr1/sony/data_dcase2022_sim
@@ -258,5 +399,20 @@ if __name__ == '__main__':
     /m/triton/scratch/work/falconr1/sony/data_dcase2022
     --dataset_list_valid
     dcase2022_devtest_all.txt
+    
+    -c
+    ./configs/run_debug.yaml
+    --dataset_trim_wavs
+    -1
+    --dataset_root
+    /m/triton/scratch/work/falconr1/sony/data_dcase2022
+    /m/triton/scratch/work/falconr1/sony/data_dcase2022
+    --dataset_list_train
+    dcase2022_devtrain_debug.txt
+    dcase2022_devtrain_debug.txt
+    --dataset_root_valid
+    /m/triton/scratch/work/falconr1/sony/data_dcase2022
+    --dataset_list_valid
+    dcase2022_devtest_debug.txt
     """
     main()
