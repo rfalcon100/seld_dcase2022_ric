5b2833a14eb82222e066d8926c8df4aa935865a1
Falcon Perez Ricardo <ricardo.falconperez@aalto.fi>
2022-08-26 14:13:06 +0300

    Plot of median and CI for bootstrapped metrics.
    Added code to make a plot that visualizes the metrics of the models.
    So this takes the bootstrapped metrics, and plots the median and
    CIS as bars.
    It looks better than the table.

--- process_results_for_tables.py
+++ process_results_for_tables.py
@@ -170,82 +170,7 @@ def prepare_evaluation_bootstrap(list_of_runs, num_classes=12, dcase2022=False):
 
     return df
 
-def aggregate_scores_bootstrap_TRASH(df, row_id, num_classes=12, dcase2022=False, n_trials=10):
-    from evaluation.dcase2022_metrics import cls_compute_seld_results, parameters
-    params = parameters.get_params()
-
-    # Compute just the DCASE 2021 final results
-    if not dcase2022:
-        score_obj = cls_compute_seld_results.ComputeSELDResults(params,
-                                                                ref_files_folder="/m/triton/scratch/work/falconr1/sony/data_dcase2021_task3/metadata_dev")
-    else:
-        score_obj = cls_compute_seld_results.ComputeSELDResults(params,
-                                                                ref_files_folder="/m/triton/scratch/work/falconr1/sony/data_dcase2022/metadata_dev")
-    print('')
-
-    df_MACRO_median = pd.DataFrame(columns=['group', 'ER', 'F', 'LE', 'LR', 'seld_score', 'class_wise'])
-    df_MACRO_cilow = pd.DataFrame(columns=['group', 'ER', 'F', 'LE', 'LR', 'seld_score', 'class_wise'])
-    df_MACRO_cihigh = pd.DataFrame(columns=['group', 'ER', 'F', 'LE', 'LR', 'seld_score', 'class_wise'])
-    df_micro_median = pd.DataFrame(columns=['group', 'ER', 'F', 'LE', 'LR', 'seld_score'])
-    df_micro_cilow = pd.DataFrame(columns=['group', 'ER', 'F', 'LE', 'LR', 'seld_score'])
-    df_micro_cihigh = pd.DataFrame(columns=['group', 'ER', 'F', 'LE', 'LR', 'seld_score'])
-
-    df_MACRO = pd.DataFrame(columns=['group', 'ER (median)', 'ER (ci_low)', 'ER (ci_hi)',
-                                     'F (median)', 'F (ci_low)', 'F (ci_hi)',
-                                     'LE (median)', 'LE (ci_low)', 'LE (ci_hi)',
-                                     'LR (median)', 'LR (ci_low)', 'LR (ci_hi)',
-                                     'seld_score (median)', 'seld_score (ci_low)', 'seld_score (ci_hi)', 'class_wise'])
-
-    df_micro = pd.DataFrame(columns=['group', 'ER (median)', 'ER (ci_low)', 'ER (ci_hi)',
-                                     'F (median)', 'F (ci_low)', 'F (ci_hi)',
-                                     'LE (median)', 'LE (ci_low)', 'LE (ci_hi)',
-                                     'LR (median)', 'LR (ci_low)', 'LR (ci_hi)',
-                                     'seld_score (median)', 'seld_score (ci_low)', 'seld_score (ci_hi)'])
-
-    groups = df['group'].unique()
-    for this_group in groups[row_id:row_id+1]:
-        paths = []
-        print(f'Group = {this_group}')
-        mask = df['group'] == this_group
-        full_paths = df[mask]['pred_path'].tolist()
-        print(f'Bootstrap analysis with {len(full_paths)} runs in group.')
-
-        seld_metric_macro, seld_metric_micro = score_obj.get_SELD_Results_Bootstrap(full_paths, num_classes=num_classes, n_trials=n_trials)
-
-        # ===================
-        # Micro metrics
-        ER_median, F_median, LE_median, LR_median, seld_scr_median = seld_metric_micro[0]
-        ER_ci_low, F_ci_low, LE_ci_low, LR_ci_low, seld_scr_ci_low = seld_metric_micro[1]
-        ER_ci_hi, F_ci_hi, LE_ci_hi, LR_ci_hi, seld_scr_ci_hi = seld_metric_micro[2]
-        df_micro = df_micro.append({'group': this_group,
-                                                  'ER (median)': ER_median, 'ER (ci_low)': ER_ci_low, 'ER (ci_hi)': ER_ci_hi,
-                                                 'F (median)': F_median, 'F (ci_low)': F_ci_low, 'F (ci_hi)': F_ci_hi,
-                                                 'LE (median)': LE_median, 'LE (ci_low)': LE_ci_low, 'LE (ci_hi)': LE_ci_hi,
-                                                 'LR (median)': LR_median, 'LR (ci_low)': LR_ci_low, 'LR (ci_hi)': LR_ci_hi,
-                                                 'seld_score (median)': seld_scr_median, 'seld_score (ci_low)': seld_scr_ci_low, 'seld_score (ci_hi)': seld_scr_ci_hi}, ignore_index=True)
-        df_micro_median = df_micro_median.append({'group': this_group,
-                                                    'ER': ER, 'F': F, 'LE': LE, 'LR': LR, 'seld_score': seld_scr}, ignore_index=True)
-        ER, F, LE, LR, seld_scr = seld_metric_micro[1]
-        df_micro_cilow = df_micro_cilow.append({'group': this_group,
-                                                    'ER': ER, 'F': F, 'LE': LE, 'LR': LR, 'seld_score': seld_scr}, ignore_index=True)
-        ER, F, LE, LR, seld_scr = seld_metric_micro[2]
-        df_micro_cihigh = df_micro_cihigh.append({'group': this_group,
-                                                    'ER': ER, 'F': F, 'LE': LE, 'LR': LR, 'seld_score': seld_scr}, ignore_index=True)
-
-        # ===================
-        # MACRO metrics
-        ER, F, LE, LR, seld_scr = seld_metric_macro[0]
-        df_MACRO_median = df_MACRO_median.append({'group': this_group, 'ER': ER, 'F': F, 'LE': LE, 'LR': LR, 'seld_score': seld_scr,
-                                                  'class_wise': None}, ignore_index=True)
-        ER, F, LE, LR, seld_scr = seld_metric_macro[1]
-        df_MACRO_cilow = df_MACRO_cilow.append({'group': this_group, 'ER': ER, 'F': F, 'LE': LE, 'LR': LR, 'seld_score': seld_scr,
-                                                  'class_wise': None}, ignore_index=True)
-        ER, F, LE, LR, seld_scr = seld_metric_macro[2]
-        df_MACRO_cihigh = df_MACRO_cihigh.append({'group': this_group, 'ER': ER, 'F': F, 'LE': LE, 'LR': LR, 'seld_score': seld_scr,
-                                                  'class_wise': None}, ignore_index=True)
-    return (df_MACRO_median, df_MACRO_cilow, df_MACRO_cihigh), (df_micro_median, df_micro_cilow, df_micro_cihigh),
-
-def aggregate_scores_bootstrap(df, row_id, num_classes=12, dcase2022=False, n_trials=10):
+def aggregate_scores_bootstrap(df, row_id=None, num_classes=12, dcase2022=False, n_trials=10):
     from evaluation.dcase2022_metrics import cls_compute_seld_results, parameters
     params = parameters.get_params()
 
@@ -271,7 +196,14 @@ def aggregate_scores_bootstrap(df, row_id, num_classes=12, dcase2022=False, n_tr
                                      'seld_score (median)', 'seld_score (ci_low)', 'seld_score (ci_hi)'])
 
     groups = df['group'].unique()
-    for this_group in groups[row_id:row_id+1]:
+
+    # Filter groups when running in parallel
+    if row_id is not None:
+        groups_list = groups[row_id:row_id+1]
+    else:
+        groups_list = groups
+
+    for this_group in groups_list:
         paths = []
         print(f'Group = {this_group}')
         mask = df['group'] == this_group
@@ -313,14 +245,156 @@ def export_individual_csvs(df_MACRO, df_micro):
     df_MACRO.to_csv(path_or_buf=f'./csvs/{filename}/df_{filename}_{params.chunk_id}_MACRO.csv')
     df_micro.to_csv(path_or_buf=f'./csvs/{filename}/df_{filename}_{params.chunk_id}_micro.csv')
 
-def export_individual_csvs_TRASH(df_MACRO, df_micro):
-    stats = ['median', 'ci_low', 'ci_high']
-    if not os.path.exists('./csvs'):
-        os.mkdir('./csvs')
-    for ii, this_df in enumerate(df_MACRO):
-        this_df.to_csv(path_or_buf=f'./csvs/df_{filename}_{params.chunk_id}_MACRO_{stats[ii]}.csv')
-    for ii, this_df in enumerate(df_micro):
-        this_df.to_csv(path_or_buf=f'./csvs/df_{filename}_{params.chunk_id}_micro_{stats[ii]}.csv')
+def plot_metrics(df_MACRO, suptitle=''):
+    stats = []
+    for index, row in df_MACRO.iterrows():
+        this_stat = {
+            "label": row['group'],  # not required
+            "mean": 0.5,  # not required
+            "med": row['seld_score (median)'],
+            "q1": row['seld_score (ci_low)'],
+            "q3": row['seld_score (ci_hi)'],
+            # "cilo": 5.3 # not required
+            # "cihi": 5.7 # not required
+            "whislo": row['seld_score (ci_low)'],  # required
+            "whishi": row['seld_score (ci_low)'],  # required
+            "fliers": []  # required if showfliers=True
+        }
+        stats.append(this_stat)
+    stats_seld = []
+    stats_er = []
+    stats_f = []
+    stats_le = []
+    stats_lr = []
+    for index, row in df_MACRO.iterrows():
+        this_stat = {
+            "label": row['group'],  # not required
+            "mean": 0.5,  # not required
+            "med": row['seld_score (median)'],
+            "q1": row['seld_score (ci_low)'],
+            "q3": row['seld_score (ci_hi)'],
+            # "cilo": 5.3 # not required
+            # "cihi": 5.7 # not required
+            "whislo": row['seld_score (ci_low)'],  # required
+            "whishi": row['seld_score (ci_low)'],  # required
+            "fliers": []  # required if showfliers=True
+        }
+        stats_seld.append(this_stat)
+        this_stat = {
+            # "label": row['group'],  # not required
+            "mean": 0.5,  # not required
+            "med": row['ER (median)'],
+            "q1": row['ER (ci_low)'],
+            "q3": row['ER (ci_hi)'],
+            # "cilo": 5.3 # not required
+            # "cihi": 5.7 # not required
+            "whislo": row['ER (ci_low)'],  # required
+            "whishi": row['ER (ci_low)'],  # required
+            "fliers": []  # required if showfliers=True
+        }
+        stats_er.append(this_stat)
+        this_stat = {
+            # "label": row['group'],  # not required
+            "mean": 0.5,  # not required
+            "med": row['F (median)'],
+            "q1": row['F (ci_low)'],
+            "q3": row['F (ci_hi)'],
+            # "cilo": 5.3 # not required
+            # "cihi": 5.7 # not required
+            "whislo": row['F (ci_low)'],  # required
+            "whishi": row['F (ci_low)'],  # required
+            "fliers": []  # required if showfliers=True
+        }
+        stats_f.append(this_stat)
+        this_stat = {
+            # "label": row['group'],  # not required
+            "mean": 0.5,  # not required
+            "med": row['LE (median)'],
+            "q1": row['LE (ci_low)'],
+            "q3": row['LE (ci_hi)'],
+            # "cilo": 5.3 # not required
+            # "cihi": 5.7 # not required
+            "whislo": row['LE (ci_low)'],  # required
+            "whishi": row['LE (ci_low)'],  # required
+            "fliers": []  # required if showfliers=True
+        }
+        stats_le.append(this_stat)
+        this_stat = {
+            # "label": row['group'],  # not required
+            "mean": 0.5,  # not required
+            "med": row['LR (median)'],
+            "q1": row['LR (ci_low)'],
+            "q3": row['LR (ci_hi)'],
+            # "cilo": 5.3 # not required
+            # "cihi": 5.7 # not required
+            "whislo": row['LR (ci_low)'],  # required
+            "whishi": row['LR (ci_low)'],  # required
+            "fliers": []  # required if showfliers=True
+        }
+        stats_lr.append(this_stat)
+    if True:
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        mpl.rcParams['text.usetex'] = False
+        plt.close('all')
+        fs = 10  # fontsize
+        fig, axes_all = plt.subplots(nrows=1, ncols=5, figsize=(24, 4), sharey=True)
+        # titles=[r'$\mathrm{ER}_{\mathrm{LD}}\downarrow$', r"$\mathrm{F}_{\mathrm{LD}}\uparrow$", r'$\mathrm{LE}_{\mathrm{CD}}\downarrow$', r'$\mathrm{LR}_{\mathrm{CD}}\uparrow$', r'$\rm{\mathcal{E}_{SELD}}\downarrow$' ]
+        ctr = 0
+        for axes, stats in zip(axes_all.flatten(), [stats_er, stats_f, stats_le, stats_lr, stats_seld]):
+            coso = axes.bxp(stats, vert=False, patch_artist=True, widths=0.6, showcaps=False, showmeans=False, showfliers=True,
+                            manage_ticks=True)
+            axes.set_title(r"$\mathrm{ER}_{\mathrm{LD}}", fontsize=fs)
+            # axes.set_title(titles[ctr], fontsize=fs)
+            ctr += 1
+            # fill with colors
+            colors = ['pink', 'lightblue', 'lightgreen']
+            cmap = mpl.cm.get_cmap('Dark2')
+            ii = 0
+            for box, median in zip(coso['boxes'], coso['medians']):
+                box.set_facecolor(cmap(ii))
+                median.set(linewidth=5, color=[0.0, 0.0, 0.0])
+                ii += 1
+            ii = 0
+            for jj in range(0, len(coso['whiskers']), 2):
+                print(jj)
+                coso['whiskers'][jj].set(color=cmap(ii))
+                coso['whiskers'][jj + 1].set(color=cmap(ii))
+                ii += 1
+            ii = 0
+            # Move left and bottom spines outward by 10 points
+            axes.spines.left.set_position(('outward', 10))
+            axes.spines.bottom.set_position(('outward', 10))
+            # Hide the right and top spines
+            axes.spines.left.set_visible(False)
+            axes.spines.right.set_visible(False)
+            axes.spines.top.set_visible(False)
+            # Only show ticks on the left and bottom spines
+            axes.yaxis.set_ticks_position('none')
+            axes.xaxis.set_ticks_position('bottom')
+            axes.xaxis.set_tick_params(width=3)
+            axes.spines.bottom.set_linewidth(2)
+            axes.grid(color=[0.75, 0.75, 0.75], linestyle='-', linewidth=2, axis='x')
+            plt.tight_layout(w_pad=4)
+        plt.suptitle(suptitle)
+        plt.show()
+    if False:
+        # testing my plot
+        import seaborn as sns
+        # Construct the DataFrame with MultiIndex:
+        df = pd.DataFrame.from_dict({"Min Value": [2, 5, 3, 4, 1, 0],
+                                     "Max Value": [10, 5, 8, 5, 11, 4]})
+        index_arr = [["A", "A", "A", "B", "B", "B"],
+                     ["Bad", "Good", "Good", "Bad", "Good", "Good"]]
+        df.index = pd.MultiIndex.from_arrays(index_arr,
+                                             names=["Category 1", "Category 2"])
+        # Use groupby to find min and max for each category
+        df_max = df.groupby(by=['Category 1', 'Category 2']).max()
+        df_min = df.groupby(by=['Category 1', 'Category 2']).min()
+        # Set the min values for the first column
+        df_max.iloc[:, 0] = df_min.iloc[:, 0]
+        # Plot the Boxplot (seems more appropriate then barplot)
+        sns.boxplot(data=df_max.T)
 
 if __name__=='__main__':
     # IMORTANT:
@@ -419,7 +493,12 @@ if __name__=='__main__':
         num_classes = 13 if params.dcase2022 else 12  # chang enum_calsses according to dataset
         df_dummy = prepare_evaluation_bootstrap(all_outputs, num_classes=num_classes, dcase2022=params.dcase2022)
         df_MACRO, df_micro = aggregate_scores_bootstrap(df_dummy, num_classes=num_classes, dcase2022=params.dcase2022, n_trials=params.n_trials)
-        export_individual_csvs(df_MACRO, df_micro)
+        #export_individual_csvs(df_MACRO, df_micro)
+
+        suptitle = ''
+        suptitle = 'MACRO, 2022, ntrials=10'
+        plot_metrics(df_MACRO, suptitle=suptitle)
+
 
     elif params.mode == 3:
         if params.chunk_id > len(all_outputs):
@@ -456,6 +535,9 @@ if __name__=='__main__':
         df_MACRO_out = pd.concat(df_MACRO_list, ignore_index=True)
         df_micro_out = pd.concat(df_micro_list, ignore_index=True)
 
+        suptitle = ''
+        #suptitle = 'micro, 2022, ntrials=1000'
+        plot_metrics(df_micro_out, suptitle=suptitle)
         # Note, I should use debug mode here, and manually copy paste the DFs to my excel for formatting
 
     print("============================")
