47a380176cc9306d404b61357222e13ad77fed7b
Falcon Perez Ricardo <ricardo.falconperez@aalto.fi>
2022-08-30 15:19:03 +0300

    Added fixed ranges for the plots, and some corrections

--- process_results_for_tables.py
+++ process_results_for_tables.py
@@ -245,7 +245,7 @@ def export_individual_csvs(df_MACRO, df_micro):
     df_MACRO.to_csv(path_or_buf=f'./csvs/{filename}/df_{filename}_{params.chunk_id}_MACRO.csv')
     df_micro.to_csv(path_or_buf=f'./csvs/{filename}/df_{filename}_{params.chunk_id}_micro.csv')
 
-def plot_metrics(df_MACRO, suptitle=''):
+def plot_metrics(df_MACRO, suptitle='', macro_range=True):
     stats = []
     for index, row in df_MACRO.iterrows():
         this_stat = {
@@ -339,29 +339,35 @@ def plot_metrics(df_MACRO, suptitle=''):
         plt.close('all')
         fs = 10  # fontsize
         fig, axes_all = plt.subplots(nrows=1, ncols=5, figsize=(24, 4), sharey=True)
-        # titles=[r'$\mathrm{ER}_{\mathrm{LD}}\downarrow$', r"$\mathrm{F}_{\mathrm{LD}}\uparrow$", r'$\mathrm{LE}_{\mathrm{CD}}\downarrow$', r'$\mathrm{LR}_{\mathrm{CD}}\uparrow$', r'$\rm{\mathcal{E}_{SELD}}\downarrow$' ]
+        titles = [r'$\mathrm{ER}_{\mathrm{LD}}\downarrow$', r"$\mathrm{F}_{\mathrm{LD}}\uparrow$", r'$\mathrm{LE}_{\mathrm{CD}}\downarrow$', r'$\mathrm{LR}_{\mathrm{CD}}\uparrow$', r'$\rm{\mathcal{E}_{SELD}}\downarrow$' ]
+        if macro_range:
+            ranges = [(0.62, 0.82), (0.24, 0.42), (18, 38), (0.36, 0.56), (0.46, 0.56)]
+        else:
+            ranges = [(0.62, 0.82), (0.38, 0.48), (14, 24), (0.38, 0.58), (0.42, 0.50)]
         ctr = 0
         for axes, stats in zip(axes_all.flatten(), [stats_er, stats_f, stats_le, stats_lr, stats_seld]):
-            coso = axes.bxp(stats, vert=False, patch_artist=True, widths=0.6, showcaps=False, showmeans=False, showfliers=True,
+            ax = axes.bxp(stats, vert=False, patch_artist=True, widths=0.6, showcaps=False, showmeans=False, showfliers=True,
                             manage_ticks=True)
-            axes.set_title(r"$\mathrm{ER}_{\mathrm{LD}}", fontsize=fs)
-            # axes.set_title(titles[ctr], fontsize=fs)
+            #axes.set_title(r"$\mathrm{ER}_{\mathrm{LD}}$", fontsize=fs)
+            axes.set_title(titles[ctr], fontsize=fs)
+            axes.set_xlim(ranges[ctr])
             ctr += 1
             # fill with colors
             colors = ['pink', 'lightblue', 'lightgreen']
-            cmap = mpl.cm.get_cmap('Dark2')
+            if len(ax['boxes']) >= 10:
+                cmap = mpl.cm.get_cmap('tab20b')
+            else:
+                cmap = mpl.cm.get_cmap('Dark2')
             ii = 0
-            for box, median in zip(coso['boxes'], coso['medians']):
+            for box, median in zip(ax['boxes'], ax['medians']):
                 box.set_facecolor(cmap(ii))
                 median.set(linewidth=5, color=[0.0, 0.0, 0.0])
                 ii += 1
             ii = 0
-            for jj in range(0, len(coso['whiskers']), 2):
-                print(jj)
-                coso['whiskers'][jj].set(color=cmap(ii))
-                coso['whiskers'][jj + 1].set(color=cmap(ii))
+            for jj in range(0, len(ax['whiskers']), 2):
+                ax['whiskers'][jj].set(color=cmap(ii))
+                ax['whiskers'][jj + 1].set(color=cmap(ii))
                 ii += 1
-            ii = 0
             # Move left and bottom spines outward by 10 points
             axes.spines.left.set_position(('outward', 10))
             axes.spines.bottom.set_position(('outward', 10))
@@ -493,11 +499,16 @@ if __name__=='__main__':
         num_classes = 13 if params.dcase2022 else 12  # chang enum_calsses according to dataset
         df_dummy = prepare_evaluation_bootstrap(all_outputs, num_classes=num_classes, dcase2022=params.dcase2022)
         df_MACRO, df_micro = aggregate_scores_bootstrap(df_dummy, num_classes=num_classes, dcase2022=params.dcase2022, n_trials=params.n_trials)
+        df_MACRO = df_MACRO.sort_values('group', ascending=True)
+        df_micro = df_micro.sort_values('group', ascending=True)
         #export_individual_csvs(df_MACRO, df_micro)
 
         suptitle = ''
-        suptitle = 'MACRO, 2022, ntrials=10'
+        suptitle = f'MACRO, {"2022" if params.dcase2022 else "2021"}, ntrials={params.n_trials}'
         plot_metrics(df_MACRO, suptitle=suptitle)
+
+        suptitle = f'micro, {"2022" if params.dcase2022 else "2021"}, ntrials={params.n_trials}'
+        plot_metrics(df_micro, suptitle=suptitle)
 
 
     elif params.mode == 3:
@@ -532,12 +543,15 @@ if __name__=='__main__':
                     df_MACRO_list.append(tmp)
                 if 'micro' in os.path.basename(this_file):
                     df_micro_list.append(tmp)
-        df_MACRO_out = pd.concat(df_MACRO_list, ignore_index=True)
-        df_micro_out = pd.concat(df_micro_list, ignore_index=True)
+        df_MACRO_out = pd.concat(df_MACRO_list, ignore_index=True).sort_values('group', ascending=True)
+        df_micro_out = pd.concat(df_micro_list, ignore_index=True).sort_values('group', ascending=True)
 
         suptitle = ''
-        #suptitle = 'micro, 2022, ntrials=1000'
-        plot_metrics(df_micro_out, suptitle=suptitle)
+        suptitle = f'MACRO, {"2022" if params.dcase2022 else "2021"}, ntrials=1000'
+        plot_metrics(df_MACRO_out, suptitle=suptitle, macro_range=True)
+
+        suptitle = f'micro, {"2022" if params.dcase2022 else "2021"}, ntrials=1000'
+        plot_metrics(df_micro_out, suptitle=suptitle, macro_range=False)
         # Note, I should use debug mode here, and manually copy paste the DFs to my excel for formatting
 
     print("============================")
